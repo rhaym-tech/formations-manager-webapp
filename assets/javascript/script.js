e0ee346b14bab10b5896c05b22277d0f6a473534
akram2803 <ahmanakram7@gmail.com>
2024-05-26 01:22:25 +0200

    mine is better

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,18 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
 /**
  * Cette fonction modifie la date des commentaires pour les afficher dans un format plus lisible.
  */
- function modifierCommentairesDates() {
-    // Sélectionne tous les éléments ayant la classe "comment-timestamp".
-    let commentTimestampElement = document.querySelectorAll("div.comment-timestamp");
-    
-    // Pour chaque élément sélectionné, modifie le contenu texte.
-    commentTimestampElement.forEach((timestamp) => {
-        // Convertit le texte de l'élément en objet Date.
-        const date = new Date(timestamp.textContent);
-        
-        // Modifie le texte de l'élément pour afficher la date et l'heure dans un format lisible.
-        timestamp.textContent = `Envoyé le ${nomDeJour(date.getDay())} ${date.toLocaleDateString("fr-FR")} à ${date.getHours() < 10 ? "0" + date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}`;
-    });
+function modifierCommentairesDates() {
+    let commentaires = document.querySelectorAll('div.comment-timestamp');
+
+    commentaires.forEach((element) => {
+        let dateString = element.textContent;
+
+        const date = new Date(dateString);
+
+        let options = { weekday: 'long', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+        let dateFormatee = date.toLocaleDateString('fr-FR', options);
+
+        element.textContent = "Envoyé le " + dateFormatee;
+    })   
 }
 
 /**
@@ -50,31 +51,4 @@ function managerDevelopersCards() {
             }
         });
     });
-}
-
-/**
- * Une fonction qui retourner le nom de jour d'apres le numero de jour
- * @param {number} numDeJour Numero de jour: Dimanche - Samedi ( 0 - 6 )
- * @returns Le nom de jour
- * @throws Un erreur si le numero de jour n'est pas valide
- */
-function nomDeJour(numDeJour) {
-    switch(numDeJour) {
-        case 0:
-            return "Dimanche";
-        case 1:
-            return "Lundi";
-        case 2:
-            return "Mardi";
-        case 3:
-            return "Mercredi";
-        case 4:
-            return "Jeudi";
-        case 5:
-            return "Vendredi";
-        case 6:
-            return "Samedi";
-        default:
-            throw new Error("Ce jour n'existe pas");
-    }
 }
