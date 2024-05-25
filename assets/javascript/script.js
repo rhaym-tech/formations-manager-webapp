72e7c63140e0017e09ee5d12ae0fa5b096029c5c
= <=>
2024-05-26 03:19:34 +0900

    akram failed me

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,29 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
     managerDevelopersCards();
 });
 
-/*
-Akram, since you wasn't able to proove yourself in HTML/CSS, I give you your last chance in making a one single JavaScript function
-
-TODO: Make a JavaScript/DOM function to format the date comments
-? The comments sent date is displayed with it's basic form from the SQL database (YYYY-MM-DD HH:MM), I want you to make a JavaScript DOM function to format the date comments in french like this:
-* Envoyé le Dimanche 26/05/2024 à 09:05
-* I'll help you by giving you things that you can use in making this function
-* 1) Date Class:
-    it's a class that can be used to get the day of the week, the month, the year, the hour, the minute, the second, and the timezone.
-    you can use it like this:
-    const date = new Date(...); // pass your arguments to it depends on what do you need
-
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const day = date.getDay();
-    const date = date.toLocaleDateString("la format de la date que vous voulez"); // exemple: "fr-FR" ou "en-US"
-
-* 2) The fonction nomDeJour(nombreDeJour) that I declared below which returns you the name of the day based on it's number, more details are explained in the function comments
-See line 78 for more details
-*/
+/**
+ * Cette fonction modifie la date des commentaires pour les afficher dans un format plus lisible.
+ */
+ function modifierCommentairesDates() {
+    // Sélectionne tous les éléments ayant la classe "comment-timestamp".
+    let commentTimestampElement = document.querySelectorAll("div.comment-timestamp");
+    
+    // Pour chaque élément sélectionné, modifie le contenu texte.
+    commentTimestampElement.forEach((timestamp) => {
+        // Convertit le texte de l'élément en objet Date.
+        const date = new Date(timestamp.textContent);
+        
+        // Modifie le texte de l'élément pour afficher la date et l'heure dans un format lisible.
+        timestamp.textContent = `Envoyé le ${nomDeJour(date.getDay())} ${date.toLocaleDateString("fr-FR")} à ${date.getHours() < 10 ? "0" + date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}`;
+    });
+}
 
 /**
- *? Cette fonction gère l'affichage des cartes de développeurs.
+ * Cette fonction gère l'affichage des cartes de développeurs.
  */
 function managerDevelopersCards() {
     // Sélectionne le bouton qui montre ou cache les cartes des développeurs.
